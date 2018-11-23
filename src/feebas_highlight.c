1330687fa1980fcaf39a247328ed9be817ad31ef
Melody <melody@pallet.town>
2018-11-23 08:44:20 -0500

    simpler but still wrong

--- src/feebas_highlight.c
+++ src/feebas_highlight.c
@@ -5,13 +5,6 @@
 
 #define NUM_FEEBAS_SPOTS    6
 
-const u16 gRoute119WaterTileSectionData[3][3] =
-{
-    {0, 0x2D, 0},
-    {0x2E, 0x5B, 0x83},
-    {0x5C, 0x8B, 0x12A},
-};
-
 void HighlightFeebasSpots() {
     u16 nWaterTiles = 447;
     u16 nFeebasGenerated = 0;
@@ -21,9 +14,6 @@ void HighlightFeebasSpots() {
     s16 x;
     s16 y;
     u16 randomTile;
-    u8 section;
-    u16 yMin;
-    u16 yMax;
     u8 index;
 
     // Seed Feebas RNG
@@ -40,24 +30,18 @@ void HighlightFeebasSpots() {
         }
     }
 
-    for(section = 0; section < 3; section++) {
-        yMin = gRoute119WaterTileSectionData[section][0];
-        yMax = gRoute119WaterTileSectionData[section][1];
-        currentWaterTile = gRoute119WaterTileSectionData[section][2];
-
-        for(y = yMin; y <= yMax; y++) {
-            for(x = 0; x < gMapHeader.mapLayout->width; x++) {
-                u8 metatileBehavior = MapGridGetMetatileBehaviorAt(x, y);
-                u8 feebasEncounterable = MetatileBehavior_IsFeebasEncounterable(metatileBehavior);
-                if (feebasEncounterable == TRUE) {
-                    currentWaterTile++;
-                    // check each feebas spot to see if it matches
-                    if(nFeebasHighlighted < NUM_FEEBAS_SPOTS) {
-                        for(index = 0; index < NUM_FEEBAS_SPOTS; index++) {
-                            if(currentWaterTile == feebasTiles[index]) {
-                                MapGridSetMetatileIdAt(x, y, 0x147);
-                                nFeebasHighlighted++;
-                            }
+    for(y = 0; y < gMapHeader.mapLayout->height; y++) {
+        for(x = 0; x < gMapHeader.mapLayout->width; x++) {
+            u8 metatileBehavior = MapGridGetMetatileBehaviorAt(x, y);
+            u8 feebasEncounterable = MetatileBehavior_IsFeebasEncounterable(metatileBehavior);
+            if (feebasEncounterable == TRUE) {
+                currentWaterTile++;
+                // check each feebas spot to see if it matches
+                if(nFeebasHighlighted < NUM_FEEBAS_SPOTS) {
+                    for(index = 0; index < NUM_FEEBAS_SPOTS; index++) {
+                        if(currentWaterTile == feebasTiles[index]) {
+                            MapGridSetMetatileIdAt(x, y, 0x147);
+                            nFeebasHighlighted++;
                         }
                     }
                 }
